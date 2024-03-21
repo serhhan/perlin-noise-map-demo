79965fcdd7801061baa99065dc8fbd79da42f3e7
Serhan Elmali <serhanelmali@gmail.com>
2024-03-21 23:59:40 +0300

    refactor: Refactor terrain color calculation in Map component

--- components/map.tsx
+++ components/map.tsx
@@ -1,36 +1,45 @@
 "use client";
 
+// Import the terrainRanges array
+import { terrainRanges } from "@/constants/terrain-ranges";
 import { useEffect, useRef } from "react";
 import p5 from "p5";
 
 const Map = () => {
   const sketchRef = useRef(null);
 
-  let zoomFactor = 100;
-
   useEffect(() => {
     const myP5 = new p5((p) => {
+      // Convert terrainRanges colors to p5 Colors
+      const p5TerrainRanges = terrainRanges.map((range) => ({
+        ...range,
+        startColor: p.color(...range.startColor),
+        endColor: p.color(...range.endColor),
+      }));
+
       p.setup = () => {
         p.createCanvas(600, 600);
         p.noLoop();
-        p.noiseSeed(11199610); // to have same noise pattern every time
+        p.noiseSeed(11199610);
       };
 
       p.draw = () => {
         p.loadPixels();
         for (let x = 0; x < p.width; x++) {
           for (let y = 0; y < p.height; y++) {
-            const noiseValue = p.noise(x / zoomFactor, y / zoomFactor);
+            const noiseValue = p.noise(x / 100, y / 100);
+            let terrainColor = p.color(0); // Default color if no range matches
 
-            let terrainColor;
-            if (noiseValue < 0.4) {
-              terrainColor = p.color(30, 176, 251);
-            } else if (noiseValue < 0.5) {
-              terrainColor = p.color(255, 246, 193);
-            } else if (noiseValue < 0.7) {
-              terrainColor = p.color(118, 239, 124);
-            } else {
-              terrainColor = p.color(22, 181, 141);
+            // Find the appropriate terrain range based on the noise value
+            for (const range of p5TerrainRanges) {
+              if (noiseValue >= range.start && noiseValue <= range.end) {
+                // Calculate the interpolation factor (t)
+                const t =
+                  (noiseValue - range.start) / (range.end - range.start);
+                // Interpolate between the startColor and endColor of the current range
+                terrainColor = p.lerpColor(range.startColor, range.endColor, t);
+                break;
+              }
             }
 
             p.set(x, y, terrainColor);
